21a06186754d3405af3c44c9830871955fda8532
dwikisry <suryadwiki4@mhs.mdp.ac.id>
2023-05-19 14:22:05 +0700

    update

--- app/src/main/java/com/ahmfarisi/kampuskita/API/RetroServer.java
+++ app/src/main/java/com/ahmfarisi/kampuskita/API/RetroServer.java
@@ -4,17 +4,16 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
-    private static final String alamatServer = "https://kampuskitadwk.000webhostapp.com/";
+    private static final String alamatServer = "https://kampuskitafrs.000webhostapp.com/";
     private static Retrofit retro;
 
     public static Retrofit konekRetrofit(){
-       if (retro == null){
-           retro = new Retrofit.Builder()
-                   .baseUrl(alamatServer)
-                   .addConverterFactory(GsonConverterFactory.create())
-                   .build();
-
-       }
-       return retro;
+        if (retro == null){
+            retro = new Retrofit.Builder()
+                    .baseUrl(alamatServer)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retro;
     }
 }
