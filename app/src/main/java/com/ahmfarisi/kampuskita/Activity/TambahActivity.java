3a1a5d808b0b87cc6827b152c04e0d63ca3fee05
dwikisry <suryadwiki4@mhs.mdp.ac.id>
2023-05-24 12:20:44 +0700

    update

--- app/src/main/java/com/ahmfarisi/kampuskita/Activity/TambahActivity.java
+++ app/src/main/java/com/ahmfarisi/kampuskita/Activity/TambahActivity.java
@@ -3,14 +3,57 @@ package com.ahmfarisi.kampuskita.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.ahmfarisi.kampuskita.API.APIRequestData;
+import com.ahmfarisi.kampuskita.API.RetroServer;
+import com.ahmfarisi.kampuskita.Model.ModelResponse;
 import com.ahmfarisi.kampuskita.R;
 
+import retrofit2.Call;
+
 public class TambahActivity extends AppCompatActivity {
+    private EditText etNama, etKota, etAlamat;
+    private Button btnTambah;
+    private String nama, kota, alamat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah);
+
+        etNama = findViewById(R.id.et_nama);
+        etKota = findViewById(R.id.et_kota);
+        etAlamat = findViewById(R.id.et_alamat);
+        btnTambah = findViewById(R.id.btn_tambah);
+
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nama = etNama.getText().toString();
+                kota = etKota.getText().toString();
+                alamat = etAlamat.getText().toString();
+
+                if(nama.trim().isEmpty()){
+                    etNama.setError("Nama Tidak Boleh Kosong!!!!");
+                }
+                else if(kota.trim().isEmpty()){
+                    etKota.setError("Kota Tidak Boleh Kosong!!!");
+                }
+                else if(alamat.trim().isEmpty()){
+                    etAlamat.setError("Alamat Tidak Boleh Kosong!!!");
+                }
+                else{
+                    tambahKampus();
+                }
+            }
+        });
+    }
+    private void tambahKampus(){
+        APIRequestData API = RetroServer.konekRetrofit().create(APIRequestData.class);
+        Call<ModelResponse> proses = API.ardCreate(nama, kota, alamat);
+
     }
 }
